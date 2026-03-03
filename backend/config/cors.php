b6053ce49fb5a5a53b63c6e569b515f100adf7ef
phuem-norng <po56191@gmail.com>
2026-03-03 09:00:48 +0700

    Update cors.php

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -20,14 +20,13 @@ return [
     'allowed_methods' => ['*'],
 
     'allowed_origins' => [
-        env('FRONTEND_URL', 'https://fitandsleek-1-0.vercel.app'),
+        env('FRONTEND_URL', 'http://localhost:5173'),
         'http://localhost:5173',
         'http://127.0.0.1:5173',
         'https://localhost:5173',
         'https://127.0.0.1:5173',
         'http://localhost:3000',
         'http://127.0.0.1:3000',
-    'https://fitandsleek-1-0.vercel.app',
     
     ],
 
